cc92a6d45a6cd965c125d8dbd2ce8f16586cd3e7
ricky-kiva <ricky.kiva@gmail.com>
2023-08-13 12:16:14 +0700

    feat(Koin): initiate Koin

---
+++ app/src/main/java/com/dicoding/mysimplelogin/StorageModule.kt
@@ -0,0 +1,12 @@
+package com.dicoding.mysimplelogin
+
+import org.koin.dsl.module
+
+val storageModule = module {
+    factory {
+        SessionManager(get())
+    }
+    factory {
+        UserRepository(get())
+    }
+}
